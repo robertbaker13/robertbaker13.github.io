8a497916ee487b0a51885ab9cf74eb1d85875fc3
Robert Baker <robertbaker13@gmail.com>
2015-07-05 10:04:40 -0700

    toy with mobile

--- mobile/index.js
+++ mobile/index.js
@@ -1,8 +1,41 @@
 $(document).ready(function() {
 
 
-  $('header').on("swipe",function(){
-    $(this).toggleClass("center left");
+  $('header').on("swipeleft",function(){
+    var currentClass = $(this).attr("class");
+    if(currentClass == "center") {
+      $(this).toggleClass("center left");
+    }
+    else if(currentClass == "right") {
+      $(this).toggleClass("center right");
+    }
+  });
+  $('header').on("swiperight",function(){
+    var currentClass = $(this).attr("class");
+    if(currentClass == "center") {
+      $(this).toggleClass("center right");
+    }
+    else if(currentClass == "left") {
+      $(this).toggleClass("center left");
+    }
+  });
+  $('footer').on("swipeleft",function(){
+    var currentClass = $(this).attr("class");
+    if(currentClass == "center") {
+      $(this).toggleClass("center left");
+    }
+    else if(currentClass == "right") {
+      $(this).toggleClass("center right");
+    }
+  });
+  $('footer').on("swiperight",function(){
+    var currentClass = $(this).attr("class");
+    if(currentClass == "center") {
+      $(this).toggleClass("center right");
+    }
+    else if(currentClass == "left") {
+      $(this).toggleClass("center left");
+    }
   });
 
 
